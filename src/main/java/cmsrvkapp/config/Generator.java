4de80d577a58c0248e3cc80f3fc853e0a386da12
MikeGus <vfc17@yandex.ru>
2018-09-29 18:55:58 +0300

    Added css generator by template and config

---
+++ src/main/java/cmsrvkapp/config/Generator.java
@@ -0,0 +1,22 @@
+package cmsrvkapp.config;
+
+import org.json.JSONObject;
+
+import java.util.Iterator;
+
+
+public class Generator {
+    static void generate(JSONObject config, JSONObject template) {
+
+        Iterator<String>  configIterator = config.keys();
+        while (configIterator.hasNext()) {
+            String configField = configIterator.next();
+            if (template.get(configField) == null) {
+                template.put(configField, config.get(configField));
+            } else {
+                generate(config.getJSONObject(configField), template.getJSONObject(configField));
+            }
+        }
+
+    }
+}
