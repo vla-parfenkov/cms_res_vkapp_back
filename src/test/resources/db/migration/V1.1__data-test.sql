ef4fe628f6fadf2a8e83108920873aa70098d6e6
MikeGus <vfc17@yandex.ru>
2018-09-29 13:26:04 +0300

    Fixed postgres path for heroku

--- src/test/resources/db/migration/V1.1__data-test.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO users (email, login, password)
-values ('Odin@mail.ru', 'Odin', '02103452'),
-  ('yaho@bb.com', 'yaho', 'qwerty'),
-  ('Bred@bb.com', 'Bred', '13213'),
-  ('Zert@mail.ru', 'Zert', '02103'),
-  ('Artur@ad.com', 'Artur', '13213');
